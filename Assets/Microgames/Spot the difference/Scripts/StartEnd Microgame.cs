bc3e0890e2b99f7ad2bfcca8765e6e81f208e216
FraxJonesy <milokathiravelu@gmail.com>
2024-04-11 16:15:50 +0800

    spot the difference
    code is working

---
+++ Assets/Microgames/Spot the difference/Scripts/StartEnd Microgame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartEndMicrogame : Microgame
+{
+
+    override protected void Start()
+    {
+        base.Start();
+    }
+    override protected void PlayMicrogame()
+    {
+        base.PlayMicrogame();
+    }
+
+    override protected void StopMicrogame()
+    {
+        base.StopMicrogame();
+    }
+}
